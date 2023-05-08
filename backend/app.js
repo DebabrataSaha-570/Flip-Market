6e4b60ba2531f2188ebdb99b678ecc1b208ecd1a
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2023-05-08 13:06:44 +0600

    Order api added

--- backend/app.js
+++ backend/app.js
@@ -12,9 +12,11 @@ app.use(cookieParser());
 //Route imports
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
+app.use("/api/v1", order);
 
 //Middleware for error
 app.use(errorMiddleware);
